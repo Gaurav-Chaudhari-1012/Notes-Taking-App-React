499f3a7142f8d6ce02eef9b67f23045e5ecba5a3
Gaurav Chaudhari <gauravchaudhari1012@gmail.com>
2024-04-11 22:24:27 +0530

    Fix line endings

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/video-app-React/",
+  base: "/Notes-Taking-App-React/",
   plugins: [react()],
 })
